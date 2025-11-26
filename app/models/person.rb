c33df567e3480126963395f14b2390999c7e71f2
harish khileri <harishkhileri98@gmail.com>
2025-11-26 16:23:46 +0530

    add has_secure_method and then apply in rails console like add password and recovery_password and then check valid? or not

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,3 +1,8 @@
 class Person
-  extend ActiveModel::Naming
+  include ActiveModel::SecurePassword
+
+  has_secure_password
+  has_secure_password :recovery_password, validations: false
+
+  attr_accessor :password_digest, :recovery_password_digest
 end

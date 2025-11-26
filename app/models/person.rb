52632eafb4071cce6ce7da6aec525bfabf89e877
harish khileri <harishkhileri98@gmail.com>
2025-11-26 13:40:11 +0530

    add conversion for methods: to_model, to_key, to_param, and to_partial_path

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,32 +1,12 @@
 class Person
-  extend ActiveModel::Callbacks
+  include ActiveModel::Conversion
+  attr_accessor :id
 
-  define_model_callbacks :update
-
-  before_update :reset_me
-  after_update :finalize_me
-  around_update :log_me
-
-  def update
-    run_callbacks(:update) do
-      puts "update method called"
-    end
+  def initialize(id)
+    @id = id
   end
 
-  private
-    def reset_me
-      puts "reset_me method: called before the update method"
-      throw :abort
-      puts "reset_me method: some code after abort"
-    end
-
-    def finalize_me
-      puts "finalize_me method: called after the update method"
-    end
-
-    def log_me
-      puts "log_me method: called around the update method"
-      yield
-      puts "log_me method: block successfully called"
-    end
+  def persisted?
+    id.present?
+  end
 end

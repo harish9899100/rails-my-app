4cffaa7b211153cf09c3c94d427ca275afc42dfd
harish khileri <harishkhileri98@gmail.com>
2025-11-26 12:43:43 +0530

    create alias attribute for name called full_name

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,25 +1,13 @@
 class Person
   include ActiveModel::AttributeMethods
-
-  attribute_method_affix prefix: "reset_", suffix: "_to_default!"
-  attribute_method_prefix "first_", "last_"
   attribute_method_suffix "_short?"
-
-  define_attribute_method "name"
+  define_attribute_methods :name
 
   attr_accessor :name
 
+  alias_attribute :full_name, :name
   private
-  def first_attribute(attribute)
-    public_send(attribute).split.first
-  end
-  def last_attribute(attribute)
-    public_send(attribute).split.last
-  end
   def attribute_short?(attribute)
     public_send(attribute).length < 5
-  end
-  def reset_attribute_to_default!(attribute)
-    public_send("#{attribute}=", "Default Name")
   end
 end

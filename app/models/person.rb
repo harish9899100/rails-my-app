14f338c5b2e43ffc8621fbd4f36c16869e61a11b
harish khileri <harishkhileri98@gmail.com>
2025-11-26 12:35:51 +0530

    define methods dynamically like prefixes and fuffixes

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,7 +1,25 @@
 class Person
-  include ActiveModel::Attributes
+  include ActiveModel::AttributeMethods
 
-  attribute :name, :string
-  attribute :date_of_birth, :date
-  attribute :active, :boolean, default: true
+  attribute_method_affix prefix: "reset_", suffix: "_to_default!"
+  attribute_method_prefix "first_", "last_"
+  attribute_method_suffix "_short?"
+
+  define_attribute_method "name"
+
+  attr_accessor :name
+
+  private
+  def first_attribute(attribute)
+    public_send(attribute).split.first
+  end
+  def last_attribute(attribute)
+    public_send(attribute).split.last
+  end
+  def attribute_short?(attribute)
+    public_send(attribute).length < 5
+  end
+  def reset_attribute_to_default!(attribute)
+    public_send("#{attribute}=", "Default Name")
+  end
 end

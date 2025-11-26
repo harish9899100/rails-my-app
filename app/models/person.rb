514ecb4ae51e0c267a712624d350b7b136e67c5a
harish khileri <harishkhileri98@gmail.com>
2025-11-26 12:55:53 +0530

    add callbacks in the lifecycle of code like define_model_callbacks :update now whenwe update in code then message showing that we define in callbacks

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,13 +1,34 @@
 class Person
-  include ActiveModel::AttributeMethods
-  attribute_method_suffix "_short?"
-  define_attribute_methods :name
+  extend ActiveModel::Callbacks
 
-  attr_accessor :name
+  define_model_callbacks :update
 
-  alias_attribute :full_name, :name
-  private
-  def attribute_short?(attribute)
-    public_send(attribute).length < 5
+  before_update :reset_me
+  after_update :finalize_me
+  around_update :log_me
+
+  # `define_model_callbacks` method containing `run_callbacks` which runs the callback(s) for the given event
+  def update
+    run_callbacks(:update) do
+      puts "update method called"
+    end
   end
+
+  private
+    # When update is called on an object, then this method is called by `before_update` callback
+    def reset_me
+      puts "reset_me method: called before the update method"
+    end
+
+    # When update is called on an object, then this method is called by `after_update` callback
+    def finalize_me
+      puts "finalize_me method: called after the update method"
+    end
+
+    # When update is called on an object, then this method is called by `around_update` callback
+    def log_me
+      puts "log_me method: called around the update method"
+      yield
+      puts "log_me method: block successfully called"
+    end
 end

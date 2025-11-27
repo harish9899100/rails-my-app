86d0fde84737e982d9c66f999f7bf043b53babe5
harish khileri <harishkhileri98@gmail.com>
2025-11-27 15:05:41 +0530

    add validation in model

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,3 +1,9 @@
 class Person
-  extend ActiveModel::Translation
+  include ActiveModel::Validations
+
+  attr_accessor :name, :email, :token
+
+  validates :name, presence: true
+  validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
+  validates! :token, presence: true
 end

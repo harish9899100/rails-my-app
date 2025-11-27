5f6efcb57e69959b3bbfdce42666fa0c86ed1e12
harish khileri <harishkhileri98@gmail.com>
2025-11-27 14:55:34 +0530

    add Translation for make more human readable

--- app/models/person.rb
+++ app/models/person.rb
@@ -1,15 +1,3 @@
 class Person
-  include ActiveModel::Serialization
-
-  attr_accessor :name, :age
-
-  def attributes
-    # Declaration of attributes that will be serialized
-    { "name" => nil, "age" => nil }
-  end
-
-  def capitalized_name
-    # Declared methods can be later included in the serialized hash
-    name&.capitalize
-  end
+  extend ActiveModel::Translation
 end

91ddb7f6ce8f9db5716308ac92e0960452246a03
harish khileri <harishkhileri98@gmail.com>
2025-12-16 15:02:28 +0530

    use resource for singular objects not id persent

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resources :practices, :users
+  resources :practices
+  resources :users
+  resource :geocoders
+  # resolve("Geocoder") { [ :geocoder ] }
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.

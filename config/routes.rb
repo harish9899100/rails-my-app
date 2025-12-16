09acb0a3a556cdd10a0ab3475d1565cb97681a9d
harish khileri <harishkhileri98@gmail.com>
2025-12-16 14:25:03 +0530

    we can defind multyple routes in on resources like resources :practice, :users

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-  get "users/index"
-  get "users/new"
-  get "/users/:id", to: "users#show"
-  get "users/create"
-  resources :practices
+  resources :practices, :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.

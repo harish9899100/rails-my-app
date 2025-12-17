a0251003e838f233189e221010cc53f4758db5dd
harish khileri <harishkhileri98@gmail.com>
2025-12-17 12:28:40 +0530

    concern we are use for declare a comman routes if we have a magazines and movies both have a ads then we are make a concern for ads and then use for magazines and movies both

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,23 @@
 Rails.application.routes.draw do
+  concern :commentable do
+    resources :ads
+  end
+  resources :movies, concerns: :commentable
+  resources :magazines, concerns: :commentable
+  # resources :movies do
+  #   resources :ads
+  # end
+  # resources :magazines do
+  #   resources :ads
+  # end
   # scope module: "admin" do
   #   resources :users
   # end
   # # resources :users, path: "/admin/users"
-  shallow do
-    resources :magazines do
-      resources :ads
-    end
-  end
+  # shallow do
+  #   resources :magazines do
+  #     resources :ads
+  #   end
+  # end
   # resources :users
 end

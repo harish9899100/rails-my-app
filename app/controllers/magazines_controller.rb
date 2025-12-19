082f2f8dce396e5fed3bf31e56695efd59bafee4
harish khileri <harishkhileri98@gmail.com>
2025-12-19 11:37:16 +0530

    update magazines_controller for ci/cd task

--- app/controllers/magazines_controller.rb
+++ app/controllers/magazines_controller.rb
@@ -4,19 +4,20 @@ class MagazinesController < ApplicationController
   end
 
   def show
-    # @magazine = Magazine.all
-    # @magazine = Magazine.find(params[:id])
-    render params[:magazines]
-  rescue ActionView::MissingTemplate
+    @magazine = Magazine.find(params[:id])
+  rescue ActiveRecord::RecordNotFound
     render file: "public/404.html", status: :not_found
   end
 
   def preview
-    @magazine = Magazine.all
+    # use a plural name for the collection to avoid confusion in the view
+    @magazines = Magazine.all
   end
+
   def new
   end
+
   def unknown
-      render plain: "Unknown magazine", status: :not_found
+    render plain: "Unknown magazine", status: :not_found
   end
 end

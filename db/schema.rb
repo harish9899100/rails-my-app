f0fb0691dc3b2354c4358f70f8b41e63cf694f1e
harish khileri <harishkhileri98@gmail.com>
2025-12-19 16:41:14 +0530

    how to use comparison validation in rails

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_12_19_100032) do
+ActiveRecord::Schema[8.0].define(version: 2025_12_19_110022) do
   create_table "ads", force: :cascade do |t|
     t.string "name"
     t.integer "magazine_id", null: false
@@ -123,6 +123,13 @@ ActiveRecord::Schema[8.0].define(version: 2025_12_19_100032) do
     t.string "name"
     t.integer "price"
     t.text "uses"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "promotions", force: :cascade do |t|
+    t.date "start_date"
+    t.date "end_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
